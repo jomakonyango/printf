a72a4c57d51065c7c0a2a0c55e3f6ae06cda19ed
John Otieno Oyango <jomakonyango@gmail.com>
2023-12-30 11:07:06 -0800

    format.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Printf Project
+1.function that produces output according to a format.
+Prototype: int _printf(const char *format, ...);
---
+++ _printf.c
@@ -0,0 +1,69 @@
+#include "main.h"
+#include <stdio.h>
+
+/* Helper function to handle %c, %s, and %d specifiers */
+int handle_specifier(int c, va_list args)
+{
+char *s;
+int count = 0;
+
+switch (c)
+{
+case 'c':
+c = va_arg(args, int);
+putchar(c);
+count++;
+break;
+case 's':
+s = va_arg(args, char *);
+while (*s)
+{
+putchar(*s++);
+count++;
+}
+break;
+case 'd':
+case 'i':
+{
+int num = va_arg(args, int);
+char str[12]; /* Buffer big enough for an int. */
+sprintf(str, "%d", num);
+for (s = str; *s; s++, count++)
+putchar(*s);
+break;
+}
+}
+
+return count;
+}
+
+/**
+* _printf - Produces output according to a format.
+* @format: A string containing the desired format.
+*
+* Return: The number of characters printed.
+*/
+int _printf(const char *format, ...)
+{
+va_list args;
+int count = 0;
+int i;
+
+va_start(args, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+putchar(format[i]);
+count++;
+}
+else
+{
+i++;
+count += handle_specifier(format[i], args);
+}
+}
+va_end(args);
+
+return (count);
+}
