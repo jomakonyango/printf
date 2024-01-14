db68f3ebf7617e5cff2abc813bcc34f4e4973335
John Otieno <jomakonyango@gmail.com>
2024-01-14 05:59:28 -0800

    conversion specifiers c s %

--- _printf.c
+++ _printf.c
@@ -3,57 +3,80 @@
 #include "main.h"
 
 /**
- * _printf - a function that produces output according to a format
- * @format: character string composed of zero or more directives
- *
- * Return: the number of characters printed (excluding the null byte)
+ * print_char - Prints a character
+ * @args: List of arguments
+ * @count: Pointer to the count of characters
  */
+void print_char(va_list args, int *count)
+{
+char c = va_arg(args, int);
+write(1, &c, 1);
+(*count)++;
+}
+
+/**
+ * print_string - Prints a string
+ * @args: List of arguments
+ * @count: Pointer to the count of characters
+ */
+void print_string(va_list args, int *count)
+{
+char *str = va_arg(args, char *);
+while (*str != '\0')
+{
+write(1, str, 1);
+str++;
+(*count)++;
+}
+}
+
+/**
+ * print_percent - Prints a percent symbol
+ * @count: Pointer to the count of characters
+ */
+void print_percent(int *count)
+{
+write(1, "%", 1);
+(*count)++;
+}
+
+/**
+* _printf - a function that produces output according to a format
+* @format: character string composed of zero or more directives
+*
+* Return: the number of characters printed (excluding the null byte)
+*/
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    const char *traverse;
-    char *str;
-    char c;
+va_list args;
+int count = 0;
+const char *traverse;
 
-    va_start(args, format);
-    for (traverse = format; *traverse != '\0'; traverse++)
-    {
-        while (*traverse != '%' && *traverse != '\0')
-        {
-            write(1, traverse, 1);
-            traverse++;
-            count++;
-        }
-        if (*traverse == '\0')
-            break;
-        traverse++;  /* skip the '%' */
-        switch (*traverse)
-        {
-            case 'c':
-            {
-                c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-                break;
-            }
-            case 's':
-            {
-                str = va_arg(args, char *);
-                while (*str != '\0')
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-                break;
-            }
-            case '%':
-                write(1, traverse, 1);
-                count++;
-                break;
-        }
-    }
-    va_end(args);
-    return (count);
+va_start(args, format);
+for (traverse = format; *traverse != '\0'; traverse++)
+{
+while (*traverse != '%' && *traverse != '\0')
+{
+write(1, traverse, 1);
+traverse++;
+count++;
+}
+if (*traverse == '\0')
+break;
+traverse++;  /* skip the '%' */
+switch (*traverse)
+{
+case 'c':
+print_char(args, &count);
+break;
+case 's':
+print_string(args, &count);
+break;
+case '%':
+print_percent(&count);
+break;
+}
+}
+va_end(args);
+return (count);
 }
