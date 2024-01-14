f7b40882e1ddf52ec7f4bb3098d132165b1d0e4c
John Otieno <jomakonyango@gmail.com>
2024-01-14 03:34:19 -0800

    conversion specifiers:

--- _printf.c
+++ _printf.c
@@ -1,58 +1,59 @@
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
-* _printf - produces output according to a format
-* @format: character string composed of zero or more directives
-*
-* Return: the number of characters printed (excluding the null byte
-* used to end output to strings)
-*/
+ * _printf - a function that produces output according to a format
+ * @format: character string composed of zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte)
+ */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+    va_list args;
+    int count = 0;
+    const char *traverse;
+    char *str;
+    char c;
 
-va_start(args, format);
-
-for (int i = 0; format[i]; i++)
-{
-if (format[i] != '%')
-{
-_putchar(format[i]);
-count++;
-}
-else
-{
-i++;
-switch (format[i])
-{
-case 'c':
-_putchar(va_arg(args, int));
-count++;
-break;
-case 's':
-char *str = va_arg(args, char *);
-for (int j = 0; str[j]; j++)
-{
-_putchar(str[j]);
-count++;
-}
-break;
-case '%':
-_putchar('%');
-count++;
-break;
-default:
-_putchar('%');
-_putchar(format[i]);
-count += 2;
-break;
-}
-}
-}
-
-va_end(args);
-
-return (count);
+    va_start(args, format);
+    for (traverse = format; *traverse != '\0'; traverse++)
+    {
+        while (*traverse != '%' && *traverse != '\0')
+        {
+            write(1, traverse, 1);
+            traverse++;
+            count++;
+        }
+        if (*traverse == '\0')
+            break;
+        traverse++;  /* skip the '%' */
+        switch (*traverse)
+        {
+            case 'c':
+            {
+                c = va_arg(args, int);
+                write(1, &c, 1);
+                count++;
+                break;
+            }
+            case 's':
+            {
+                str = va_arg(args, char *);
+                while (*str != '\0')
+                {
+                    write(1, str, 1);
+                    str++;
+                    count++;
+                }
+                break;
+            }
+            case '%':
+                write(1, traverse, 1);
+                count++;
+                break;
+        }
+    }
+    va_end(args);
+    return (count);
 }
